98071de3139035cc30cb58070620a50fa2b82614
wuhongye <1253697896@qq.com>
2017-02-15 18:06:03 +0800

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,10 @@
 class Book < ActiveRecord::Base
 
-  validates :title, presence: { message: "标题不能为空" }
-  validates :content, presence: { message: "内容不能为空" }
   validates :user_id, presence: { message: "用户不能为空" }
+  validates :meal, presence: { message: "菜名不能为空" }
+  validates :times, presence: { message: "具体时间" }
+  validates :day, presence: { message: "上/下午" }
+  validates :payer, presence: { message: "付款人" }
 
   belongs_to :user
   has_many :books_tags, class_name: "BooksTags"
