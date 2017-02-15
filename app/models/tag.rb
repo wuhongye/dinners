3609a50ef1bfba6a3755a20d3aa59eb85d58ac4b
wuhongye <1253697896@qq.com>
2017-02-15 16:19:02 +0800

    update

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
-class Tag < ApplicationRecord
+class Tag < ActiveRecord::Base
+
+  validates :title, uniqueness: { message: "标签已存在" }
+
+  has_many :books_tags, class_name: "BooksTags"
+  has_many :books, through: :books_tags
+
 end
